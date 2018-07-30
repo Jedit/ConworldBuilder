b38c418858c203449e1341d27c56b0ed0f68dbb4
Jedit <jedit@seznam.cz>
2018-07-30 13:45:42 +0200

    ICalendar

---
+++ ConworldBuilder/ConworldBuilder/Model/Timeline/CalendarInterface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConworldBuilder.Model.Timeline {
+    interface ICalendar {
+        TimePoint GetTimePoint(string timedate);
+        TimePoint GetTimePoint(IEnumerable<string> timedate);
+
+        string GetTimeDate(TimePoint timePoint);
+    }
+}
---
+++ ConworldBuilder/ConworldBuilder/Model/Timeline/TimePoint.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConworldBuilder.Model.Timeline {
+    class TimePoint {
+        protected long day;
+        protected int time;
+
+        public long Day {
+            get {
+                return day;
+            }
+            set {
+                day = value;
+            }
+        }
+
+        public int Time {
+            get {
+                return time;
+            }
+            set {
+                time = value;
+            }
+        }
+    }
+}
