15d50602374047e1eb012ce17f5cfe1774e41a02
Jedit <jedit@seznam.cz>
2018-07-30 13:57:07 +0200

    TimeInterval

---
+++ ConworldBuilder/ConworldBuilder/Model/Helper/IntervalType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConworldBuilder.Model.Helper {
+    /// <summary>
+    /// Indicates if the interval include end points.
+    /// </summary>
+    enum IntervalType {
+        InIn,
+        InEx,
+        ExIn,
+        ExEx
+    }
+}
---
+++ ConworldBuilder/ConworldBuilder/Model/Timeline/TimeInterval.cs
@@ -0,0 +1,41 @@
+﻿using ConworldBuilder.Model.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConworldBuilder.Model.Timeline {
+    class TimeInterval {
+        protected TimePoint start;
+        protected TimePoint end;
+        protected IntervalType type;
+
+        public TimePoint Start {
+            get {
+                return start;
+            }
+            set {
+                start = value;
+            }
+        }
+
+        public TimePoint End {
+            get {
+                return end;
+            }
+            set {
+                end = value;
+            }
+        }
+
+        public IntervalType Type {
+            get {
+                return type;
+            }
+            set {
+                type = value;
+            }
+        }
+    }
+}
